343bb8df031e5afa982b40fd2713c3445fe42d59
Loui Recio <lreciopersonal@gmail.com>
2023-09-27 08:05:31 -0500

    feat(backReady)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{Router, routing::get};
+
+
+#[tokio::main]
+
+async fn main() {
+    let app = Router::new().route("/", get(index));
+    let address = SocketAddr::from(([0, 0, 0, 0], 5050));
+    axum::Server::bind(&address)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+
 }
+async fn index() -> &'static str {
+    "Hello, World!"
+}
+
+
